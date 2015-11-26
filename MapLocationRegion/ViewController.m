8a1014047ac3506bf253c2ffaa982ddf03de6756
Neylor <nwsbagagi@gmail.com>
2015-11-26 17:55:48 -0200

    initial commit
    - get user location
    - show update user location
    - ask authorisation for location service during app use
    - location service max accuracy

--- MapLocationRegion/ViewController.m
+++ MapLocationRegion/ViewController.m
@@ -7,16 +7,57 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
-
+@interface ViewController () <CLLocationManagerDelegate>
+@property (weak, nonatomic) IBOutlet UILabel *latitudeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *longitudeLabel;
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    CLLocationManager *locationManager;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    locationManager = [[CLLocationManager alloc]init];
+    [locationManager setDelegate:self];
+    
+    switch ([[locationManager class] authorizationStatus]) {
+        case kCLAuthorizationStatusDenied:
+            NSLog(@"kCLAuthorizationStatusDenied");
+            break;
+        case kCLAuthorizationStatusRestricted:
+            NSLog(@"kCLAuthorizationStatusRestricted");
+            break;
+        case kCLAuthorizationStatusNotDetermined:
+            NSLog(@"kCLAuthorizationStatusNotDetermined");
+            [locationManager requestWhenInUseAuthorization];
+            break;
+        case kCLAuthorizationStatusAuthorizedWhenInUse:
+            NSLog(@"kCLAuthorizationStatusAuthorizedWhenInUse");
+            
+            [locationManager setDesiredAccuracy:kCLLocationAccuracyBest];
+            [locationManager setDistanceFilter:kCLDistanceFilterNone];
+            
+            [_latitudeLabel setText:[NSString stringWithFormat:@"%f",locationManager.location.coordinate.latitude]];
+            [_longitudeLabel setText:[NSString stringWithFormat:@"%f",locationManager.location.coordinate.longitude]];
+            
+            [locationManager startUpdatingLocation];
+            break;
+        case kCLAuthorizationStatusAuthorizedAlways:
+            NSLog(@"kCLAuthorizationStatusAuthorizedAlways");
+            break;
+        default:
+            break;
+    }
+    
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations{
+    [_latitudeLabel setText:[NSString stringWithFormat:@"%f",locations.lastObject.coordinate.latitude]];
+    [_longitudeLabel setText:[NSString stringWithFormat:@"%f",locations.lastObject.coordinate.longitude]];
 }
 
 - (void)didReceiveMemoryWarning {
